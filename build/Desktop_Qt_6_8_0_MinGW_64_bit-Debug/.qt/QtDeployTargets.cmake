00d745039b15f41924acab54c8c2290f36f9f229
Urga7 <urban.gajsek@gmail.com>
2024-11-13 15:43:41 +0100

    initial commit

---
+++ build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_SimpleCalendar_FILE C:/Github/SimpleCalendar/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/SimpleCalendar.exe)
+set(__QT_DEPLOY_TARGET_SimpleCalendar_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_SimpleCalendar_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
